6089026606db155a8884bf6ee2e661cf836f77e5
Shane <chatpisit.khamsing@gmail.com>
2025-12-10 21:58:32 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num;
+    int even = 0, odd = 0;
+
+    while (true) {
+        cout << "Enter an integer: ";
+        cin >> num;
+
+        if (num == 0) {
+            break;   // หยุดเมื่อเจอ 0
+        }
+
+        if (num % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
+
     return 0;
 }
